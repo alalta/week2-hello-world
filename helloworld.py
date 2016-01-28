6fffe8a5eb215fcf63823aeb159d12bb8d3892ea
Anadia Lalta <alalta@mail.valenciacollege.edu>
2016-01-28 18:29:54 -0500

    fixed

--- helloworld.py
+++ helloworld.py
@@ -6,13 +6,13 @@ Portuguese = 'Ola a todos'
 
 print('Hello everyone!')
 print(' Choose a language and I will greet you in that language!')  #ask for the language they want me to greet them in 
-print('Spanish')   
-print('Italian')    
-print('Portuguese')
+print(str(1)+ '.Spanish')   
+print(str(2)+ '.Italian')    
+print(str(3) + '.Portuguese')
 language= input()
-if language == 'Spanish' :      #translates "Hello everyone" to Spanish
+if language == str(1) :      #translates "Hello everyone" to Spanish
     print('Hola a todos')
-elif language == 'Italian' :         #translates "Hello everyone" to Italian
+elif language == str(2) :         #translates "Hello everyone" to Italian
     print('Ciao a tutti')
-elif language == 'Portuguese' :       #translates "Hello everyone to Portuguese
+elif language == str(3) :       #translates "Hello everyone to Portuguese
      print('Ola a todos')
