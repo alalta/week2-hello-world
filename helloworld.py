bae69644a60e966b24ddf5232bdbcf0d108e2f7d
Anadia Lalta <alalta@mail.valenciacollege.edu>
2016-01-27 23:30:27 -0500

    updated

--- helloworld.py
+++ helloworld.py
@@ -8,7 +8,8 @@ print('Hello everyone!')
 print(' Choose a language and I will greet you in that language!')  #ask for the language they want me to greet them in 
 print('Spanish')   
 print('Italian')    
-print('Portuguese') 
+print('Portuguese')
+language= input()
 if language == 'Spanish' :      #translates "Hello everyone" to Spanish
     print('Hola a todos')
 elif language == 'Italian' :         #translates "Hello everyone" to Italian
